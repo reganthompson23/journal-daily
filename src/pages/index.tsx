da25ed282e4a2df89743747f1aabd3b36168c087
reganthompson23 <regan@syndicatestore.com.au>
2024-11-05 22:20:31 +0000

    Initial commit

---
+++ src/pages/index.tsx
@@ -0,0 +1,9 @@
+import JournalEntry from "../components/JournalEntry";
+
+export default function Home() {
+  return (
+    <main className="min-h-screen bg-background">
+      <JournalEntry />
+    </main>
+  )
+}
